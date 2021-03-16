a5e9eecaaec70004e061e278935e20fd87308d2d
Sai Vinay Varikooty <sv.varikooty@mail.utoronto.ca>
2021-03-16 15:07:13 -0400

    Updated App.tsx
    Pushed latest version and removed Section component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Container, Box, Typography } from '@material-ui/core'
 import Homepage from './components/Homepage.component'
 import FormTemplate from './components/FormTemplate.component'
+import Section from './components/Section.component'
 
 const App: React.FC = () => (
   <Container maxWidth='md'>
     <Box my={4}>
       <FormTemplate sendForm={(_) => {}} dateCreated={Date.now} />
+    </Box>
+    <Box my={4}>
+      <Homepage />
     </Box>
     <Box my={4}>
       <Homepage />
