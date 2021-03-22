f9f172e45cf33f8ec79838b93fbcf9c4502314be
Julia Cwiek <49696712+juliacwiek@users.noreply.github.com>
2021-03-22 17:36:33 -0400

    Minor form endpoint test & clean up (#79)
    * minor test to check failure of creating form
    
    * fix spacing and indentation (clean up)

--- server/__tests__/routesSmoke.test.ts
+++ server/__tests__/routesSmoke.test.ts
@@ -1,25 +1,45 @@
 import fetch from 'cross-fetch';
 
-test('Test Hello World endpoint', async   () => {
+test('Test Hello World endpoint', async () => {
   await expect(fetch('http://localhost:8080').then(response => response.text())).resolves.toBe('Hello World!');  
 });
 
-  test('Test create new form endpoint', async   () => {
+  test('Test create new form endpoint', async () => {
 
-    const data = {'title': 'testTitle', 'desc': 'testDescription', 'sections': [] as any[]}
+    const data1 = {'title': 'testTitle', 'desc': 'testDescription', 'sections': [] as any[]}
 
-    const response = (await fetch('http://localhost:8080/formTemplate?test=true',
-    {method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-      },
-    body: JSON.stringify(data)
-    }))
+    const response1 = (await fetch(
+      'http://localhost:8080/formTemplate?test=true',
+      { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data1)
+      }
+    ))
 
-    expect(response.status).toBe(200);
+    expect(response1.status).toBe(200);
+
+    const data2 = {'title': 'testTitle', 'sections': [] as any[]}
+
+    const response2 = (await fetch(
+      'http://localhost:8080/formTemplate?test=true', 
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data2)
+      }
+    ))
+
+    // data2 form missing description, therefore response should be 500
+    expect(response2.status).toBe(500);
+
   });
 
-  test('Test get all new forms endpoint', async   () => {
+  test('Test get all new forms endpoint', async () => {
 
     const response = (await fetch('http://localhost:8080/formResponse/newForms',
     {method: 'GET',
